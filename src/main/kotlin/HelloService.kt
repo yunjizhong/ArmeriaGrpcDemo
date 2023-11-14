d58f75bb79aaa1600f4256a284142c79047cb6cd
yunjizhong <yunji.zhong@doordash.com>
2023-11-14 09:50:50 -0800

    delay

--- src/main/kotlin/HelloService.kt
+++ src/main/kotlin/HelloService.kt
@@ -1,9 +1,11 @@
 import com.example.grpc.hello.HelloRequest
 import com.example.grpc.hello.HelloResponse
 import com.example.grpc.hello.HelloServiceGrpcKt
+import kotlinx.coroutines.delay
 
 class HelloService : HelloServiceGrpcKt.HelloServiceCoroutineImplBase() {
     override suspend fun hello(request: HelloRequest): HelloResponse {
+        delay(200)
         return HelloResponse.newBuilder().setMessage("Hello ${request.name}").build()
     }
 }
